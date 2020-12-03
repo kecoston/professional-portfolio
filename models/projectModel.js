1631c446b60ad0b18a7a38b7ffee0b0f344f8225
Kathryn Sherazi <67169418+kecoston@users.noreply.github.com>
2020-12-02 20:43:39 -0600

    added project seeds

--- models/projectModel.js
+++ models/projectModel.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const projectSchema = new mongoose.Schema({
-  Name: {
+  name: {
     type: String,
     required: true
   },
-  Description: {
+  description: {
     type: String,
     default: ""
   },
