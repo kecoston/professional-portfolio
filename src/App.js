ae2eb2690258df33b9c06f490cca056c15854361
Kathryn Sherazi <67169418+kecoston@users.noreply.github.com>
2020-12-04 18:18:08 -0600

    updating files for deployment

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import ProjectContainer from "./components/ProjectContainer/ProjectContainer"
+import Contact from './components/Contact/Contact'
+import Jumbotron from "./components/Jumbotron/Jumbotron"
+import About from "./components/About/About"
+
+
+function App() {
+  return (
+    <div className="container">
+      <div className="content-wrapper">
+
+      <Jumbotron></Jumbotron>
+      <About></About>
+      <ProjectContainer></ProjectContainer>
+      
+      </div>
+
+      <Contact></Contact>
+     
+    </div>
+  );
+}
+
+export default App;
