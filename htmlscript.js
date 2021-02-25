867ad9a856e578277100bab5368f89831a329962
Kathryn Sherazi <67169418+kecoston@users.noreply.github.com>
2021-02-24 19:05:08 -0600

    updated images and script for html examples

--- htmlscript.js
+++ htmlscript.js
@@ -1,71 +1,72 @@
 // Web Development 
 
-// Html CSS cards 
-// var htmlCssProjects = [
-//     {   projectName: "Horiseon",
-//         imageUrl:"images/portfolio-img/horiseon-icon.png",
-//         description: "HTML & CSS",
-//         liveSite: "https://kecoston.github.io/Horiseon-Homepage/",
-//         gitHub: "https://github.com/kecoston/"
-//     },
-//     {
-//         projectName:"Generate Password",
-//         imageUrl: "images/portfolio-img/generate-password-icon.png", 
-//         description: "HTML, CSS, Google Fonts, & Javascript",
-//         liveSite: "https://kecoston.github.io/generate-password/",
-//         gitHub: "https://github.com/kecoston/generate-password"
-//     },
-//     {
-//         projectName:"Moodii",
-//         imageUrl: "images/portfolio-img/moodii-icon.png", 
-//         description: " HTML, CSS, Bulma, Google Fonts, The MovieDatabase API, & JQuery",
-//         liveSite: "https://fullstackzack.github.io/moodii/",
-//         gitHub: "https://github.com/FullStackZack/moodii"
-//     },
-//     {
-//         projectName:"Relay",
-//         imageUrl: "images/portfolio-img/relay-icon.png", 
-//         description: "HTML, CSS, Material-UI, React.js, Passport.js, bcrypt, Mongoose, Express.js, and Vonage API",
-//         liveSite: "https://relay-app-2020.herokuapp.com/",
-//         gitHub: "https://github.com/kecoston/relay"
-//     }
-//     ]
+// Html CSS Examples 
+var htmlCssProjects = [
+    {   projectName: "Horiseon",
+        imageUrl:"images/portfolio-img/horiseon-cover.jpg",
+        description: "HTML & CSS",
+        liveSite: "https://kecoston.github.io/Horiseon-Homepage/",
+        gitHub: "https://github.com/kecoston/"
+    },
+    {
+        projectName:"Generate Password",
+        imageUrl: "images/portfolio-img/generate-password.jpg", 
+        description: "HTML, CSS, Google Fonts, & Javascript",
+        liveSite: "https://kecoston.github.io/generate-password/",
+        gitHub: "https://github.com/kecoston/generate-password"
+    },
+    {
+        projectName:"Moodii",
+        imageUrl: "images/portfolio-img/moodii-project.jpg", 
+        description: " HTML, CSS, Bulma, Google Fonts, The MovieDatabase API, & JQuery",
+        liveSite: "https://fullstackzack.github.io/moodii/",
+        gitHub: "https://github.com/FullStackZack/moodii"
+    },
+    {
+        projectName:"Relay",
+        imageUrl: "images/portfolio-img/relay.jpg", 
+        description: "HTML, CSS, Material-UI, React.js, Passport.js, bcrypt, Mongoose, Express.js, and Vonage API",
+        liveSite: "https://relay-app-2020.herokuapp.com/",
+        gitHub: "https://github.com/kecoston/relay"
+    }
+    ]
 
-// for (var i = 0; i < htmlCssProjects.length; i++) {
+for (var i = 0; i < htmlCssProjects.length; i++) {
 
-// varProjectCard = htmlCssProjects [i]
+var projectCard = htmlCssProjects[i]
+console.log(projectCard)
+console.log(projectCard.imageUrl)
 
-// var cardEl = $("<div>")
-// var cardImage = $("<img>")
-// var cardBody = $("<div>")
-// var cardTitle = $("<h5>")
-// var cardText = $("<p>")
-// var cardBtn = $("<button>")
-// var cardBtnTwo = $("<button>")
-// var col = $("<div>")
+var example = $("<article>");
+var title = $("<h3>");
+var background = $("<img>");
+var exampleDescription = $("<p>");
+var live = $("<button>");
+var git = $("<button>");
 
+example.addClass("example")
+title.addClass("example-title")
+background.addClass("example-background")
+exampleDescription.addClass("example-descript")
+live.addClass("btn btn-outline-light")
+git.addClass("btn btn-outline-light")
 
-// cardEl.addClass("card")
-// cardImage.addClass("card-img-top")
-// cardBody.addClass("card-body")
-// cardTitle.addClass("card-title")
-// cardText.addClass("p")
-// cardBtn.addClass("btn btn-outline-secondary").text("Live Site")
-// cardBtnTwo.addClass("btn btn-outline-secondary").text("GitHub Repository")
-// col.addClass("col")
-
-// cardImage.attr("src", htmlCssProjects[i].imageUrl)
-// cardTitle.text(htmlCssProjects[i].projectName)
-// cardText.text(htmlCssProjects[i].description)
-// cardBtn.attr("href", htmlCssProjects[i].liveSite)
-// cardBtnTwo.attr("href", htmlCssProjects[i].gitHub)
+live.text("Live Site")
+git.text("GitHub Repository")
 
 
-// cardEl.append(cardImage, cardTitle, cardText, cardBtn, cardBtnTwo)
-// col.append(cardEl)
-// $("#html-examples").append(col)
+background.attr("src", projectCard.imageUrl)
+title.text(projectCard.projectName)
+exampleDescription.text(projectCard.description)
+live.attr("href", projectCard.liveSite)
 
-// }
+git.attr("href", projectCard.gitHub)
+
+
+example.append(background, title, exampleDescription, live, git)
+$("#html-examples").append(example)
+
+}
 
 $("#html-examples").hide();
 
